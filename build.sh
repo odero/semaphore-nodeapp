3ba4feeb38d0be2a78cf93c56a50186fd53ff671
Billy <billyx5@gmail.com>
2017-11-13 15:13:32 +0000

    Testing out cfn deploy speed vs ecs deploy script

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ echo "Building image..."
 docker build -t $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION .
 echo "Pushing image"
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION
-echo "Deploying..."
-ecs deploy $AWS_CLUSTER $AWS_SERVICE -i $AWS_TASK $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION --timeout 600
-# echo "Updating CFN"
-# aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
-#  --parameters ParameterKey=DockerImageURL,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION \
-#  ParameterKey=DesiredCapacity,UsePreviousValue=true \
-#  ParameterKey=InstanceType,UsePreviousValue=true \
-#  ParameterKey=MaxSize,UsePreviousValue=true \
-#  ParameterKey=SubnetIDs,UsePreviousValue=true \
-#  ParameterKey=VpcId,UsePreviousValue=true
+#echo "Deploying..."
+#ecs deploy $AWS_CLUSTER $AWS_SERVICE -i $AWS_TASK $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION --timeout 600
+echo "Updating CFN"
+aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
+  --parameters ParameterKey=DockerImageURL,ParameterValue=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION \
+  ParameterKey=DesiredCapacity,UsePreviousValue=true \
+  ParameterKey=InstanceType,UsePreviousValue=true \
+  ParameterKey=MaxSize,UsePreviousValue=true \
+  ParameterKey=SubnetIDs,UsePreviousValue=true \
+  ParameterKey=VpcId,UsePreviousValue=true
